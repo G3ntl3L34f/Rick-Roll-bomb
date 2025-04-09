85699475bd54b96a6b1e5028e4f567b9459f87ea
Gentle Leaf <147330000+G3ntl3L34f@users.noreply.github.com>
2025-04-09 18:55:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import webbrowser
+import subprocess
+from time import sleep
+while True:
+    webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    subprocess.run(["hehe.exe"])
+    sleep(5)
